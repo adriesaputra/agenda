fbf38dc18d45d60045e65f357c011489bf311124
adriesaputra <adrie.saputra@gmail.com>
2024-01-09 16:34:37 +0700

    ubah home screen

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Admin\ProtokolController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
